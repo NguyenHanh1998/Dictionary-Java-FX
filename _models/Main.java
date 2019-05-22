da9b30a6786a78261a6e628e252b0cca0f482bec
Nguyen Kien <36306640+onlyno2@users.noreply.github.com>
2019-05-22 17:00:06 +0700

    add Dictionary models

---
+++ _models/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main( String args[] ) {
+        Dictionary dictionary = new Dictionary("Anh-Viet");
+
+        dictionary.insertWord("hi", "chao");
+    }
+}
