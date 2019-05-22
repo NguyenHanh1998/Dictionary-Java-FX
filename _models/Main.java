0752869935b161c502ddbb9b7fece32c1cc7cb34
Saegusa <20162230@student.hust.edu.vn>
2019-05-22 21:14:49 +0700

    remove Main.java

--- _models/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main( String args[] ) {
-        Dictionary dictionary = new Dictionary("Anh-Viet");
-
-        dictionary.insertWord("hi", "chao");
-    }
-}
